2b2f889e4f0bf9c669f556bc84e60f9d49b2ce42
Aleksandra <aleksandra.rieben@hotmail.com>
2024-08-01 11:40:29 +0200

    Change xml configuration to annotation-based configuration, add property files

--- src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -3,6 +3,7 @@ package ru.otus.hw.dao;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
@@ -12,16 +13,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
+@Component
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
         String fileName = this.fileNameProvider.getTestFileName();
         var inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
 
