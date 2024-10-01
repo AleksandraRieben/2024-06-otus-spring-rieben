2851c51b3c55a65eabadebfbaed318573f4d4992
Aleksandra <aleksandra.rieben@hotmail.com>
2024-10-01 22:33:02 +0200

    Add hw01 and hw02 to the project

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,12 +3,8 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
-import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
-
-import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +12,6 @@ public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
     private final QuestionDao questionDao;
-    private final AtomicInteger correctAnswers = new AtomicInteger();
-    private List<Question> questions;
 
     @Override
     public TestResult executeTestFor(Student student) {
@@ -34,7 +28,7 @@ public class TestServiceImpl implements TestService {
                 ioService.printLine(answer.text());
             });
             int selectedAnswer = ioService.readIntForRange(0, question.answers().size(), "error");
-            isAnswerValid = question.answers().get(selectedAnswer - 1).isCorrect();
+            isAnswerValid = question.answers().get(selectedAnswer-1).isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,22 +1,36 @@
-//package ru.otus.hw.dao;
-//
-//import org.junit.jupiter.api.Test;
-//import ru.otus.hw.config.TestFileNameProvider;
-//import ru.otus.hw.domain.Question;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//import static org.mockito.Mockito.mock;
-//
-//class CsvQuestionDaoTest {
-//    @Test
-//    void findAll() {
-//        var testFileNameProvider = mock(TestFileNameProvider.class);
-//        var repository = new CsvQuestionDao(testFileNameProvider);
-//        List<Question> questions = repository.findAll();
-//        assertNotNull(repository);
-//        assertEquals("Is there life on Mars?", questions.get(0).text());
-//    }
-//}
+package ru.otus.hw.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+class CsvQuestionDaoTest {
+    @Mock
+    private TestFileNameProvider fileNameProvider;
+
+    @InjectMocks
+    private CsvQuestionDao csvQuestionDao;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testFindAll_FileNotFound_ThrowsException() {
+        // Arrange
+        when(fileNameProvider.getTestFileName()).thenReturn("non_existing_file.csv");
+
+        // Act & Assert
+        assertThrows(QuestionReadException.class, () -> {
+            csvQuestionDao.findAll();
+        });
+    }
+}
